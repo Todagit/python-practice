c5ea616c77f33291e904c92ee4b6c8cb430d36a1
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-29 11:49:09 +0900

    Update script.py

--- script.py
+++ script.py
@@ -1,44 +1,27 @@
-def validate(hand):
-    if hand < 0 or hand > 2:
-        return False
-    return True
-
-def print_hand(hand, name='ゲスト'):
-    hands = ['グー', 'チョキ', 'パー']
-    print(name + 'は' + hands[hand] + 'を出しました')
-
-# 関数judgeを定義してください
-def judge(player, computer):
-    # playerとcomputerの比較結果によって条件を分岐してください
-    if player == computer:
-        return '引き分け'
-    elif player == 0 and computer == 1:
-        return '勝ち'
-    elif player == 1 and computer == 2:
-        return '勝ち'
-    elif player == 2 and computer == 0:
-        return '勝ち'
-    else:
-        return '負け'
+# utils.pyをモジュールとして読み込んでください
+import utils
 
 print('じゃんけんをはじめます')
 player_name = input('名前を入力してください：')
 print('何を出しますか？（0: グー, 1: チョキ, 2: パー）')
 player_hand = int(input('数字で入力してください：'))
 
-if validate(player_hand):
+# utilsモジュール内の関数validateを呼び出してください
+if utils.validate(player_hand):
     computer_hand = 1
-   
+
     if player_name == '':
-        print_hand(player_hand)
+        # utilsモジュール内の関数print_handを呼び出してください
+        utils.print_hand(player_hand)
     else:
-        print_hand(player_hand, player_name)
+        # utilsモジュール内の関数print_handを呼び出してください
+        utils.print_hand(player_hand, player_name)
+
+    # utilsモジュール内の関数print_handを呼び出してください
+    utils.print_hand(computer_hand, 'コンピュータ')
     
-    print_hand(computer_hand, 'コンピューター')
-    
-    # 変数resultに関数judgeの戻り値を代入してください
-    result = judge(player_hand, computer_hand)
-    # 変数resultを出力してください
+    # utilsモジュール内の関数judgeを呼び出してください
+    result = utils.judge(player_hand, computer_hand)
     print('結果は' + result + 'でした')
 else:
     print('正しい数値を入力してください')
