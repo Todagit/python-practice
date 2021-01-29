3513b6dcef580757521735406adc588b050209e9
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-29 11:54:17 +0900

    Update script.py

--- script.py
+++ script.py
@@ -1,26 +1,23 @@
-# utils.pyをモジュールとして読み込んでください
 import utils
+# randomモジュールを読み込んでください
+import random
 
 print('じゃんけんをはじめます')
 player_name = input('名前を入力してください：')
 print('何を出しますか？（0: グー, 1: チョキ, 2: パー）')
 player_hand = int(input('数字で入力してください：'))
 
-# utilsモジュール内の関数validateを呼び出してください
 if utils.validate(player_hand):
-    computer_hand = 1
-
+    # randintを用いて0から2までの数値を取得し、変数computer_handに代入してください
+    computer_hand = random.randint(0,2)
+    
     if player_name == '':
-        # utilsモジュール内の関数print_handを呼び出してください
         utils.print_hand(player_hand)
     else:
-        # utilsモジュール内の関数print_handを呼び出してください
         utils.print_hand(player_hand, player_name)
 
-    # utilsモジュール内の関数print_handを呼び出してください
-    utils.print_hand(computer_hand, 'コンピュータ')
+    utils.print_hand(computer_hand, 'コンピューター')
     
-    # utilsモジュール内の関数judgeを呼び出してください
     result = utils.judge(player_hand, computer_hand)
     print('結果は' + result + 'でした')
 else:
