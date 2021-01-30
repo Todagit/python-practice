5eb06f8eeab4553b83b69e80bdccad9d119b2d3e
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-30 15:33:36 +0900

    Create script4.py

---
+++ python_4/script4.py
@@ -0,0 +1,19 @@
+class MenuItem:
+    def info(self):
+        return self.name + ': ¥' + str(self.price)
+
+    # get_total_priceメソッドを定義してください
+    
+
+
+menu_item1 = MenuItem()
+menu_item1.name = 'サンドイッチ'
+menu_item1.price = 500
+
+print(menu_item1.info())
+
+# get_total_priceメソッドを呼び出してください
+
+
+# 「合計は〇〇円です」となるように出力してください
+
