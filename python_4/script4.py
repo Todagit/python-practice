bc1110d4931a991db18a7cb073f9fd5f4c8503a0
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-30 15:38:40 +0900

    Update script4.py

--- python_4/script4.py
+++ python_4/script4.py
@@ -1,8 +1,11 @@
 class MenuItem:
+    # __init__メソッドを定義してください
+    def __init__(self):
+        print('MenuItemクラスのインスタンスが生成されました！')
+
     def info(self):
         return self.name + ': ¥' + str(self.price)
 
-    # get_total_priceメソッドを定義してください
     def get_total_price(self, count):
         total_price = self.price * count
         return total_price
@@ -14,9 +17,5 @@ menu_item1.price = 500
 
 print(menu_item1.info())
 
-# get_total_priceメソッドを呼び出してください
 result = menu_item1.get_total_price(4)
-
-# 「合計は〇〇円です」となるように出力してください
 print('合計は' + str(result) + '円です')
-
