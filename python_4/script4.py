3cf3ea1d39938e661cafd89a215170625840068d
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-02-04 09:43:20 +0900

    Update script4.py

--- python_4/script4.py
+++ python_4/script4.py
@@ -1,21 +1,5 @@
-class MenuItem:
-    # 引数「name」と「price」を受け取るようにしてください
-    def __init__(self, name, price):
-        # 「サンドイッチ」の代わりに引数nameの値を代入してください
-        self.name = name
-        
-        # 「500」の代わりに引数priceの値を代入してください
-        self.price = price
+from menu_item import MenuItem
 
-    def info(self):
-        return self.name + ': ¥' + str(self.price)
-
-    def get_total_price(self, count):
-        total_price = self.price * count
-        return total_price
-
-
-# 引数を「サンドイッチ」と「500」としてください
 menu_item1 = MenuItem('サンドイッチ',500)
 
 print(menu_item1.info())
