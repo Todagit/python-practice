12df222943911fb0d8b323670ad4cfb117742139
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-02-04 09:50:32 +0900

    Update script4.py

--- python_4/script4.py
+++ python_4/script4.py
@@ -5,9 +5,14 @@ menu_item2 = MenuItem('チョコケーキ', 400)
 menu_item3 = MenuItem('コーヒー', 300)
 menu_item4 = MenuItem('オレンジジュース', 200)
 
-# 指定されたリストを変数menu_itemsに代入してください
 menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
 
-# for文を作成してください
+# 変数indexを定義し、数値の「0」を代入してください
+index = 0
+
 for item in menu_items:
-    print(item.info())
+    # 「0. サンドイッチ: ¥500」となるように出力してください
+    print(str(index) + '.' +item.info())
+    
+    # 変数indexに1を加えてください
+    index += 1
