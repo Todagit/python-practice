68ca83ce9668214ef3e38da7963fd6f13eea0dfd
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-02-06 22:01:29 +0900

    Update script4.py

--- python_4/script4.py
+++ python_4/script4.py
@@ -7,12 +7,19 @@ menu_item4 = MenuItem('オレンジジュース', 200)
 
 menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
 
-# 変数indexを定義し、数値の「0」を代入してください
 index = 0
 
 for item in menu_items:
-    # 「0. サンドイッチ: ¥500」となるように出力してください
-    print(str(index) + '.' +item.info())
-    
-    # 変数indexに1を加えてください
+    print(str(index) + '. ' + item.info())
     index += 1
+
+print('--------------------')
+
+# コンソールから入力を受け取り、変数orderに代入してください
+order = int(input('メニューの番号を入力してください: '))
+
+# 選択されたメニューを変数selected_menuに代入してください
+selected_menu = menu_items[order]
+
+# 「選択されたメニュー: 〇〇」と出力してください
+print('選択されたメニュー: ' + selected_menu.name)
