388ebbe9c0b7319a64e59d635c63d236ce44b1da
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-02-24 04:52:57 +0900

    Update script5.py

--- python_5/script5.py
+++ python_5/script5.py
@@ -3,6 +3,13 @@ from drink import Drink
 
 food1 = Food('サンドイッチ', 500)
 food1.calorie = 330
-
-# food1に対してinfoメソッドを呼び出して戻り値を出力してください
 print(food1.info())
+
+# Drinkクラスのインスタンスを生成して変数drink1に代入してください
+drink1 = Drink('コーヒー', 300)
+
+# drink1のamountに「180」を代入してください
+drink1.amount = 180
+
+# drink1に対してinfoメソッドを呼び出して戻り値を出力してください
+print(drink1.info())
