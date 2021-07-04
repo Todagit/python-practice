fdba1303f10400e1b9ab3fb9fde6dbfbf142c8c9
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-07-04 11:35:38 +0900

    Update script.py

--- python_2/script.py
+++ python_2/script.py
@@ -1,3 +1,5 @@
+# 1
+
 # 変数 fruits に、複数の文字列を要素に持つリストを代入してください
 fruits = ['apple', 'banana', 'orange'] 
 
@@ -6,3 +8,19 @@ print(fruits[0])
 
 # インデックス番号が 2 の要素を文字列と連結して出力してください
 print('好きな果物は' + fruits[2] + 'です')
+
+# 2
+
+fruits = ['apple', 'banana', 'orange']
+
+# リストの末尾に文字列「 grape 」を追加してください
+fruits.append('grape')
+
+# 変数 fruits に代入したリストを出力してください
+print(fruits)
+
+# インデックス番号が 0 の要素を文字列「 cherry 」に更新してください
+fruits[0] = 'cherry'
+
+# インデックス番号が 0 の要素を出力してください
+print(fruits[0])
