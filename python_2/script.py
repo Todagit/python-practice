58138806b34b1c7cc1ceca32404504ea485db43c
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-07-09 07:24:23 +0900

    Update script.py

--- python_2/script.py
+++ python_2/script.py
@@ -60,3 +60,12 @@ fruits['grape'] = 500
 
 # fruits の値を出力してください
 print(fruits)
+
+
+# 6
+
+fruits = {'apple': 'りんご', 'banana': 'バナナ', 'grape': 'ぶどう'}
+
+# for 文を用いて、辞書のキーを1つずつ取り出し、繰り返しの中で「 ◯◯は△△という意味です 」と出力させてください
+for fruit_key in fruits:
+    print( fruit_key + 'は' + fruits[fruit_key]+ 'という意味です')
