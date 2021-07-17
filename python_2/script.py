64a26f8431459e1098183ca08a87dc655c59d2b2
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-07-17 09:29:06 +0900

    Update script.py

--- python_2/script.py
+++ python_2/script.py
@@ -102,3 +102,17 @@ for number in numbers:
     if number % 3 == 0:
         continue
     print(number)
+
+
+
+# 10
+
+# 文字列のキーと数値の値を持つ辞書を作って、変数 items に代入してください
+items = {'apple': 100, 'banana': 200, 'orange': 400}
+
+# for 文を用いて、辞書 items のキーを1つずつ取り出していく繰り返し処理を作成してください
+for item_name in items:
+    # 「 --------------------------------------------- 」を出力してください
+    print('---------------------------------------------')
+    # 「 ◯◯は1個△△円です 」となるように出力してください
+    print(item_name + 'は1個' + str(items[item_name]) + '円です')
